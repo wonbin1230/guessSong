b709c1ab64ecf42168dc2f3a8498646214e9add2
Yu <wonbin12300@gmail.com>
2022-09-21 11:34:53 +0800

    fix: 按鈕跑版

--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const mainController = require("../controller/mainController");
 
-router.get("/",  (req, res) => {
-  res.render("guessSong", { title: "GuessSongs" });
-});
+// router.get("/",  (req, res) => {
+//   res.render("guessSong", { title: "GuessSongs" });
+// });
 
 router.route("/")
   .get(mainController.login);
